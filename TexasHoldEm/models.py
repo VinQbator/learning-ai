e4ed70efc9d6a0eff7c0a2b4a38e3b52d14ac348
Ubuntu <ubuntu@ip-172-31-10-105.ec2.internal>
2019-02-16 13:16:10 +0000

    hyperparameter tuning

--- TexasHoldEm/models.py
+++ TexasHoldEm/models.py
@@ -1,6 +1,18 @@
 from keras.models import Sequential, load_model
 from keras.layers import Dense, Flatten, Dropout
 
+def test_model(window_length, n_observations, n_actions):
+    model = Sequential(name='test-%s' % n_observations)
+    model.add(Flatten(input_shape=(window_length, n_observations)))
+    model.add(Dense(64, activation='relu'))
+    model.add(Dropout(0.4))
+    model.add(Dense(32, activation='relu'))
+    model.add(Dropout(0.4))
+    model.add(Dense(16, activation='relu'))
+    model.add(Dropout(0.4))
+    model.add(Dense(n_actions, activation='softmax'))
+    return model
+
 def simple_model(window_length, n_observations, n_actions):
     model = Sequential(name='simple-%s' % n_observations)
     model.add(Flatten(input_shape=(window_length, n_observations)))
