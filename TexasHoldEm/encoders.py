bfc0fb9873b19f32006618d55bed7a5bbf1fb6a4
Ingvar Lond <ingvar.lond@gmail.com>
2019-02-18 22:10:26 +0200

    encoder fixes

--- TexasHoldEm/encoders.py
+++ TexasHoldEm/encoders.py
@@ -1,6 +1,6 @@
 import numpy as np
 from holdem.utils import action_table, player_table, community_table
-from treys import Deck
+from treys import Deck, Card, Evaluator
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.compose import ColumnTransformer
 
@@ -12,22 +12,22 @@ class Encoder():
         community_table.MINRAISE,
         community_table.TO_CALL]
 
-    pot_normalized_player = [player_table.STACK, player_table.LAST_SIDEPOT]
+    pot_normalized_player = [
+        player_table.STACK, 
+        player_table.LAST_SIDEPOT]
 
-    def __init__(self, n_seats, ranking_encoding='one-hot'):
+    def __init__(self, n_seats, ranking_encoding='norm'):
         self.n_seats = n_seats
         self.ranking_encoding = ranking_encoding
-        #self._init_transformers()
-        self.n_dim = 265 + 104 + 6 + n_seats + 6 * n_seats #+ (7463 if ranking_encoding == 'one-hot' else 1)
+        self.n_dim = 265 + 104 + 6 + n_seats + 6 * n_seats + (7463 if ranking_encoding == 'one-hot' else 1 if ranking_encoding == 'norm' else 0)
         self._deck = np.array(Deck.GetFullDeck(), dtype=np.int64)
         self._deck_alt = np.concatenate((np.array([-1], dtype=np.int64), self._deck))
+        self._evaluator = Evaluator()
 
     def encode(self, player_states, community_infos, community_cards, our_seat, concat=True):
         player_infos, player_hands = zip(*player_states)
         player_infos = np.array(player_infos, dtype=np.float32)
-        player_hands = np.array(player_hands, dtype=np.int64)
         community_infos = np.array(community_infos, dtype=np.float32)
-        community_cards = np.array(community_cards, dtype=np.int64)
 
         n_players = player_infos.shape[0]
 
@@ -50,107 +50,31 @@ class Encoder():
         players_info_t = np.zeros((n_players, 6))
         players_info_t[:] = player_infos[:,:]
 
-        if concat:
-            return np.concatenate(
-                (community_infos_t.flatten(), players_info_t.flatten(), community_cards_t.flatten(), player_cards_t.flatten()), 
-                axis=0)
-
-        return community_infos_t, players_info_t, community_cards_t, player_cards_t#, hand_t
-
-        
-
-    def encode_slow(self, player_states, community_infos, community_cards, our_seat):
-        player_infos, player_hands = zip(*player_states)
-        player_infos = np.array(player_infos, dtype=np.float32)
-        player_hands = np.array(player_hands, dtype=np.float32)
-        community_infos = np.array(community_infos, dtype=np.float32)
-        community_cards = np.array(community_cards, dtype=np.float32)
-
-        player_index = int(community_infos[community_table.TO_ACT_POS])
-        n_players = len(player_infos)
-
-        hand = [player_infos[player_index][player_table.HAND_RANK]]
-        if self.ranking_encoding == 'one-hot':
-            hand = self._handrank_transformer.fit_transform([hand])
-        elif self.ranking_encoding == 'norm':
-            hand = [[hand[0] / 7642.0]]
+        if self.ranking_encoding is None:
+            hand = []
         else:
-            raise Exception('Unknown ranking encoding!')
+            community_cards = [card for card in community_cards if card > 0]
+            if len(community_cards) > 0:
+                hand_rank = self._evaluator.evaluate(cards, community_cards)
+            else:
+                hand_rank = -1
+
+            if self.ranking_encoding == 'norm':
+                if hand_rank > 0:
+                    hand = [1 / hand_rank]
+                else:
+                    hand = [-1]
+            elif self.ranking_encoding == 'one-hot':
+                hand = [0] * 7643
+                hand[hand_rank] = 1
+            else:
+                raise Exception('Unknown ranking encoding!')
 
         hand_t = np.array(hand)
 
-        full_stack = community_infos[community_table.BIG_BLIND] * 100
-        
-        community_infos[Encoder.pot_normalized_community] = community_infos[Encoder.pot_normalized_community] / full_stack
-        community_infos[community_table.BUTTON_POS] = (community_infos[community_table.BUTTON_POS] + our_seat) % n_players # Shift seats
+        if concat:
+            return np.concatenate(
+                (community_infos_t.flatten(), players_info_t.flatten(), community_cards_t.flatten(), player_cards_t.flatten(), hand_t.flatten()), 
+                axis=0)
 
-        community_infos_t = self._community_info_transformer.fit_transform(community_infos.reshape(1, -1))
-        community_cards_t = self._community_cards_transformer.fit_transform(community_cards.reshape(1, -1))
-
-        cards = player_hands[player_index]
-        player_cards_t = self._player_cards_transformer.fit_transform(cards.reshape(1, -1))
-
-        start = player_infos[:-our_seat] # Shift seats
-        end = player_infos[-our_seat:]
-        player_infos = np.concatenate((end, start), axis=0)
-
-        players_info_t = np.array([])
-        for info in player_infos:
-            info[player_table.SEAT_ID] = (info[player_table.SEAT_ID] + our_seat) % n_players # Shift seats
-            info[Encoder.pot_normalized_player] = info[Encoder.pot_normalized_player] / full_stack
-            players_info_t = np.append(players_info_t, self._player_info_transformer.fit_transform([info]))
-
-        return np.concatenate((community_infos_t.flatten(),
-            community_cards_t.flatten(),
-            player_cards_t.flatten(),
-            players_info_t.flatten(),
-            hand_t.flatten()),
-            axis=0)
-
-    def _init_transformers(self):
-        # WAY slower than doing manually with numpy
-
-        card_columns = [0, 1]
-        community_cards_columns = [0, 1, 2, 3, 4]
-        position_columns = [community_table.BUTTON_POS]
-        drop_positions = [community_table.TO_ACT_POS]
-        seat_columns = [player_table.SEAT_ID]
-        hand_columns = [player_table.HAND_RANK]
-        
-        deck = Deck.GetFullDeck()
-        extended_deck = [-1, *deck]
-
-        self._player_info_transformer = ColumnTransformer(
-            transformers=[(
-                'seat',
-                OneHotEncoder(categories=np.repeat(np.array([range(self.n_seats)]), len(seat_columns), axis=0), handle_unknown='ignore'), 
-                seat_columns),
-                ('drop_hand', 'drop', hand_columns)],
-            remainder='passthrough')
-
-        self._player_cards_transformer = ColumnTransformer(
-            transformers=[(
-                'cards', 
-                OneHotEncoder(categories=np.repeat(np.array([deck]), 2, axis=0), handle_unknown='ignore', sparse = False), 
-                card_columns)],
-            remainder='passthrough')
-
-        self._community_info_transformer = ColumnTransformer(
-            transformers=[(
-                'positions', 
-                OneHotEncoder(categories=np.repeat(np.array([range(self.n_seats)]), len(position_columns), axis=0), handle_unknown='ignore'), 
-                position_columns),
-                ('drop_to_act', 'drop', drop_positions)],
-            remainder='passthrough')
-
-        self._community_cards_transformer = ColumnTransformer(
-            transformers=[(
-                'cards', 
-                OneHotEncoder(categories=np.repeat(np.array([extended_deck]), 5, axis=0), handle_unknown='ignore', sparse = False), 
-                community_cards_columns)],
-            remainder='passthrough')
-    
-        self._handrank_transformer = OneHotEncoder(
-            categories=np.repeat(np.array([[-1, *range(1, 7463)]]), 1, axis=0), 
-            handle_unknown='ignore', 
-            sparse = False)
+        return community_infos_t, players_info_t, community_cards_t, player_cards_t, hand_t
--- TexasHoldEm/test.py
+++ TexasHoldEm/test.py
@@ -10,7 +10,7 @@ from encoders import Encoder
 import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
-N_PLAYERS = 10
+N_PLAYERS = 2
 
 def main():
     env = gym.make('TexasHoldem-v1', n_seats=N_PLAYERS, max_limit=100000, all_in_equity_reward=True, equity_steps=1000, debug=False)
@@ -21,23 +21,34 @@ def main():
     state = env.reset()
     #print(state)
     (player_states, (community_infos, community_cards)) = state
-    print('player states', player_states)
-    print('community info', community_infos)
-    encoder = Encoder(N_PLAYERS, ranking_encoding='norm')
+    #print('player states', player_states)
+    #print('community info', community_infos)
+    encoder = Encoder(N_PLAYERS, ranking_encoding=None)
     start_time = time.time()
     #community_info, players_info, community_cards, player_cards = encoder.encode(player_states, community_infos, community_cards, 0, concat=False)
     encoded_state = encoder.encode(player_states, community_infos, community_cards, 0)
     time_taken = time.time() - start_time
     print(time_taken)
-    print(encoded_state.shape)
+    #print(encoded_state.shape)
     tr_env = TrainingEnv.build_environment('asd', N_PLAYERS)
-    print(tr_env.n_observation_dimensions)
+    #print(tr_env.n_observation_dimensions)
     #print('Community Info:', community_info)
     #print('Players Info:', players_info)
     #print('Community Cards:', community_cards)
     #print('Player Cards:', player_cards)
     #print('Hand Rank:', hand_rank)
     #print(encoder.encode_slow(player_states, community_infos, community_cards, 0))
+    step = [[1, 0]] * N_PLAYERS
+    #print(step)
+    state, reward, done, info = env.step(step)
+    (player_states, (community_infos, community_cards)) = state
+    encoded_state = encoder.encode(player_states, community_infos, community_cards, 0)
+
+    step = [[0, 0]] * N_PLAYERS
+    state, reward, done, info = env.step(step)
+    (player_states, (community_infos, community_cards)) = state
+    encoded_state = encoder.encode(player_states, community_infos, community_cards, 0)
+
 
 if __name__ == '__main__':
     main()
